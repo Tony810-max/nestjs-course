e3045dffadf42dbf58a71f87ae7078fb7a9a33b6
tony810-max <quangvu37781@gmail.com>
2026-03-13 11:29:57 +0700

    --amend

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -8,6 +8,6 @@ import { PrismaPg } from '@prisma/adapter-pg';
 export class PrismaService extends PrismaClient {
   constructor() {
     const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-    super({ adapter });
+    super({ adapter, log: ['info'] });
   }
 }
